71588a0a91ccc3d8a3b4c9fc8a6a6ec86e9e43a7
Valerii Pozhydaiev <p_vf@i.ua>
2023-04-14 20:14:06 +0300

    12

--- src/js/header.js
+++ src/js/header.js
@@ -1,9 +1,14 @@
+import { api } from '../helpers/api';
 
-
+console.log(
+  api.getCharacters({
+    nameStartsWith: 'Hulk',
+    limit: 3,
+  })
+);
 
 const characterIn = document.querySelector('#character-input');
 const liCharacter = document.querySelector('.character-list');
-
 
 const characterOut = document.querySelector('#character-output');
 console.log(characterIn);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './helpers/api.js';
-import './js/character.js';
+// import './js/character.js';
 import './comics/comics.js';
 import './js/header';
 
