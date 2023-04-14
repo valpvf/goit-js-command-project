27ea43f28c0981d23d8a99cb9e9cfbc415a4e582
valpvf <p_vf@i.ua>
2023-04-14 08:47:17 +0000

    Deploying to gh-pages from @ valpvf/goit-js-command-project@89b0d79ab36b2ef7bd3dcde7101bad629c22d6d9 🚀

---
+++ assets/comics-4f58329c.js
@@ -0,0 +1,1 @@
+import"./styles-fac82e35.js";import{a as o}from"./api-1cf60fb1.js";console.log(o.getCharacters({}));
--- assets/page-2-693dfb14.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./api-9a52c924.js";console.log(a.getCharacters({}));
