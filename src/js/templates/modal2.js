8809bf51233db5145357fe6a9956d28ded63e85c
ViktorOsu <123021878+ViktorOsu@users.noreply.github.com>
2023-04-15 13:20:00 +0300

    template js

---
+++ src/js/templates/modal2.js
@@ -0,0 +1,95 @@
+export const createModal = obj => {
+  return `
+      <div class="backdrop" data-modal="">
+  <div class="modal">
+    <div class="modal-hero">
+      <button class="modal-close-btn" data-modal-close="">
+        <svg class="modal-close-icon" width="20" height="20">
+          <use class="icon" href="./img/icons.svg#close-icon"></use>
+        </svg>
+      </button>
+      <img
+        class="modal-main-img"
+        src="./img/modal/star.jpg"
+        alt="star"
+        class="star-photo"
+      />
+      <div class="modal-slide">
+        <ul class="modal-slide-list">
+          <li>
+            <img
+              class="modal-slide-img"
+              src="./img/modal/crawl.jpg"
+              alt="crewl"
+              width="80"
+              hieght="80"
+            />
+          </li>
+          <li>
+            <img
+              class="modal-slide-img"
+              src="./img/modal/talk.jpg"
+              alt="talk"
+              width="80"
+              hieght="80"
+            />
+          </li>
+          <li>
+            <img
+              class="modal-slide-img"
+              src="./img/modal/on-knee.jpg"
+              alt="on-knee"
+              width="80"
+              hieght="80"
+            />
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <section class="comics">
+      <div class="comics-title-date">
+        <h1 class="comics-title">Black Widow</h1>
+        <p class="comics-date">July 9, 2021</p>
+      </div>
+      <p class="comics-description">
+        {{A deadly assassin is closing in on Natasha Romanoff. Now Natasha must
+        reunite with an unlikely group of spies from her past in order to
+        survive and stop a lethal force from being unleashed on the world.}}
+      </p>
+      <h2 class="comics-list-title">List of comics</h2>
+      <ul class="comics-list">
+        <li class="comics-list-item">
+          <img
+            class="comics-list-el"
+            src="./img/modal/item-one.jpg"
+            alt="one"
+          />
+          <h3 class="comics-list-movie">Black Widow (2020)</h3>
+          <p class="comics-movie-hero">Kelly Thompson</p>
+        </li>
+        <li class="comics-list-item">
+          <img
+            class="comics-list-el"
+            src="./img/modal/item-two.jpg"
+            alt="two"
+          />
+          <h3 class="comics-list-movie">Black Widow (2020)</h3>
+          <p class="comics-movie-hero">Kelly Thompson</p>
+        </li>
+        <li class="comics-list-item">
+          <img
+            class="comics-list-el"
+            src="./img/modal/item-three.jpg"
+            alt="three"
+          />
+          <h3 class="comics-list-movie">Black Widow (2020)</h3>
+          <p class="comics-movie-hero">Kelly Thompson</p>
+        </li>
+      </ul>
+    </section>
+  </div>
+</div>
+
+    `;
+};
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import handlebars from 'vite-plugin-handlebars';
+//import handlebars from 'vite-plugin-handlebars';
 // import { resolve } from 'path'
 
 export default defineConfig({
@@ -18,5 +18,5 @@ export default defineConfig({
     },
     outDir: '../dist',
   },
-  plugins: [injectHTML(), FullReload(['./src/**/**.html']), handlebars()],
+  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
 });
