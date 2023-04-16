17f0c3986f1fed6be8eb1fd5447d2e945a78eaec
Anna-Mariia <122801694+bulo4ka032@users.noreply.github.com>
2023-04-16 16:24:52 +0300

    slider with dinamic usage

--- src/js/randomchar.js
+++ src/js/randomchar.js
@@ -1,53 +1,10 @@
 import { api } from '../helpers/api.js';
-import Swiper from 'swiper';
-import 'swiper/css';
+const rcImgList = document.querySelector('.rc-list');
+const rcDescrList = document.querySelector('.rc-descr-list');
 
-const swiper = new Swiper ('.swiper-container', 
-	{
-    speed:1000,
-		direction: 'vertical',
-    zoom: true,
-		keyboard: 
-		{
-			enabled: true,
-			onlyInViewport: false,
-		},
-		mousewheel: 
-		{
-			invert: true,
-		},
-    autoplay: 
-    {
-      delay: 2000,
-      disableOnInteraction: false,
-    },
-    loop: true,
-
-    slidesPerView: 1,
-    spaceBetween: 0,
-    centeredSlides: true,
-	}); 
-// const swiper = new Swiper('.swiper-container', {
-//   slidesPerView: 1,
-//   spaceBetween: 0,
-//   centeredSlides: true,
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: '.right-slider',
-//     prevEl: '.left-slider',
-//     },
-//   loop: true,
-//     autoplay: {
-//       delay: 3000,
-//       disableOnInteraction: true,
-//   },
-
-// });
-const rcList = document.querySelector('.rc-list');
-
+let currentSlide = 0;
+let slides = null;
+let slidesText = null;
 const getRandomOffset = () => {
   return Math.round(Math.random() * 1561);
 };
@@ -59,71 +16,62 @@ const getRandomFive = async () => {
       limit: 1,
       offset: getRandomOffset(),
     });
-    if (result[0].thumbnail.path === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available') {
+    if (
+      result[0].thumbnail.path ===
+      'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available' || !result[0].description
+    ) {
       i -= 1;
-      continue
+      continue;
     }
     results.push(result[0]);
   }
-  console.log(results)
-   randomMarkup(results);
+  console.log(results);
+  rcImgList.innerHTML = randomImgMarkup(results);
+  rcDescrList.innerHTML = randomDeskrMarkup(results);
+  slides = document.querySelectorAll('.rc-list .slide');
+  slidesText = document.querySelectorAll('.rc-descr-item');
+  slides[0].className = 'slide rc-item showing';
+  slidesText[0].className = 'rc-descr-item rc-descr-active';
+  console.log(slides[0].className);
+  let slideInterval = setInterval(nextSlide, 3500);
 };
 
-function randomMarkup(array) {
+function randomImgMarkup(array) {
   const markup = array
-    .map(({ id, thumbnail, name, description }) => {
-      return `  <li class='rc-item swiper-slide'>
-      <picture class="rc-img">
+    .map(({ id, thumbnail }) => {
+      return `  <li class='rc-item slide' data-id="${id}">
+      <picture>
       <source media="(min-width: 1440px)" srcset="${thumbnail.path}/portrait_uncanny.${thumbnail.extension}" />
       <source media="(min-width: 375px)" srcset="${thumbnail.path}/standard_fantastic.${thumbnail.extension}"/>
-      <img data-set="${id}"
+      <img class="rc-img" data-set="${id}"
         src='${thumbnail.path}/portrait_uncanny.${thumbnail.extension}'
         alt=''
-      /></picture>
-      <ul class='rc-descr-list'>
-        <li class='rc-descr-item'>
-          <h3 data-set="${id}" class='rc-descr-title'>${name}</h3>
-          <p  data-set="${id}"class='rc-descr-text'>${description}</p>
-        </li>
-      </ul>
+      /></picture>`;
+    })
+    .join('');
+  return markup;
+}
+
+function randomDeskrMarkup(array) {
+  const markup = array
+    .map(({ name, description, id }) => {
+      return `
+    <li class='rc-descr-item' data-id="${id}">
+      <h3 class='rc-descr-title'>${name}</h3>
+      <p class='rc-descr-text'>${description}</p>
     </li>`;
     })
     .join('');
-  rcList.innerHTML = markup;
+  return markup;
 }
- // console.log(markup);
-
-
-// function renderMarkup(markup) {
-//   rcList.insertAdjacentHTML(markup)
-// }
-
-//   <div class="swiper">
-//   <!-- Additional required wrapper -->
-//   <div class="swiper-wrapper">
-//     <!-- Slides -->
-//     <div class="swiper-slide">Slide 1</div>
-//     <div class="swiper-slide">Slide 2</div>
-//     <div class="swiper-slide">Slide 3</div>
-//     ...
-//   </div>
-//   <!-- If we need pagination -->
-//   <div class="swiper-pagination"></div>
-
-//   <!-- If we need navigation buttons -->
-//   <div class="swiper-button-prev"></div>
-//   <div class="swiper-button-next"></div>
-
-//   <!-- If we need scrollbar -->
-//   <div class="swiper-scrollbar"></div>
-// </div>
-
-// '.swiper', {
-//   // Optional parameters
-//   direction: 'vertical',
-//   loop: true,
-//   function randomDescrMarkup(array) {
-
-//   }
 
 getRandomFive();
+
+function nextSlide() {
+  console.log(slides[currentSlide]);
+  slides[currentSlide].className = 'slide rc-item';
+  slidesText[currentSlide].className = 'rc-descr-item';
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].className = 'slide rc-item showing';
+  slidesText[currentSlide].className = 'rc-descr-item rc-descr-active';
+}
