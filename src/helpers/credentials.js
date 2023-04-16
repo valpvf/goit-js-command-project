2195b1d362c2b57896f2fc8d620ee452fe9adef6
Valerii Pozhydaiev <p_vf@i.ua>
2023-04-16 20:18:10 +0300

    comment untimed key

--- src/helpers/credentials.js
+++ src/helpers/credentials.js
@@ -3,14 +3,14 @@ const keys = [
     publickey: 'b7033e2ec32dc004fef1d6b078a0e881',
     privatekey: 'e84d5f752e66a362d2f542b2ec296e48a27fb2a0',
   },
-  {
-    publickey: 'caba337208b47569b1f84f369984c4a2',
-    privatekey: 'b496be0246e5deafeb43afa6abcf6a04e1453022',
-  },
-  {
-    publickey: '2d66dc4d241b1e725a65709fbddb3fff',
-    privatekey: '7bcdbaf715b35f3ca9f820b168f7904a5457f407',
-  },
+  //   {
+  //     publickey: 'caba337208b47569b1f84f369984c4a2',
+  //     privatekey: 'b496be0246e5deafeb43afa6abcf6a04e1453022',
+  //   },
+  //   {
+  //     publickey: '2d66dc4d241b1e725a65709fbddb3fff',
+  //     privatekey: '7bcdbaf715b35f3ca9f820b168f7904a5457f407',
+  //   },
   {
     publickey: '2948b8e0caf400a2826fafe939e0b4f9',
     privatekey: 'cd4405cc83dbf1a338db18b1461414480be3621f',
