68e1a883030bb0ca64d885d23abfe3bc122e3664
AlexSBolotov <122620169+AlexSBolotov@users.noreply.github.com>
2023-04-18 21:17:09 +0300

    header_comics_characters

--- src/js/header.js
+++ src/js/header.js
@@ -6,7 +6,6 @@ import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
 
-
 const refs = {
   paginationEl: document.querySelector('.tui-pagination'),
   mainContainer: document.querySelector('.container'),
@@ -47,90 +46,84 @@ const paginationOptions = {
 };
 const pagination = new Pagination(refs.paginationEl, paginationOptions);
 
-
 let query = '';
 let lightbox = null;
 // let page = 1;
 const perPage = 40;
 let result = [];
 
-
 const headerInput = document.querySelector('.header-form');
 const headerColor = document.querySelector('.header');
-const headerFindResult =document.querySelector('.header-find');
+const headerFindResult = document.querySelector('.header-find');
 
 const characterOut = document.querySelector('.header-output');
 const headerIcon = document.querySelector('.header-icon');
 const headerBtn = document.querySelector('.header-btn');
 
-async function addInput (event) {
+async function addInput(event) {
   event.preventDefault();
   const { target: formEl } = event;
   query = formEl.elements.searchQuery.value;
   // headerInput.reset();
   console.log(query);
   refs.paginationEl.classList.remove('is-hidden');
-  
+
   if (query.trim() == '') {
     refs.paginationEl.classList.add('is-hidden');
     console.log('Please specify your search query.');
     return;
   }
-    // const result = await  api.getAllCharacters({nameStartsWith: query})
+  // const result = await  api.getAllCharacters({nameStartsWith: query})
+  console.log(itemsPerPage);
+  try {
+    const result = await api.getAllCharacters({
+      // comics: final,
+      limit: itemsPerPage,
+      offset: 0,
+      nameStartsWith: query,
+    });
+    // pagination.reset(res.total);
     console.log(itemsPerPage);
-    try {
-      const result = await api.getAllCharacters({
-        // comics: final,
-        limit: itemsPerPage,
-        offset: 0,
-        nameStartsWith: query,
-      });
-      // pagination.reset(res.total);
-      console.log(itemsPerPage);
-      console.log(result.results.length);
-      if (result.results.length == 0) {
+    console.log(result.results.length);
+    if (result.results.length == 0) {
       refs.paginationEl.classList.add('is-hidden');
       headerFindResult.innerHTML = '<span class="char-error-main"></span>';
-      console.log(
-      'Search result is zero. Change your query',
-      );
-        return;
-      }
-      console.log('itemsPerPage====================');
-      headerFindResult.innerHTML = '';
-      // Loading.remove();
-      createGallery(result.results);
-      pagination.reset(result.total);
-      console.log('itemsPerPage====================');
-      pagination.on('beforeMove', async evt => {
-        const { page } = evt;
+      console.log('Search result is zero. Change your query');
+      return;
+    }
+    console.log('itemsPerPage====================');
+    headerFindResult.innerHTML = '';
+    // Loading.remove();
+    createGallery(result.results);
+    pagination.reset(result.total);
+    console.log('itemsPerPage====================');
+    pagination.on('beforeMove', async evt => {
+      const { page } = evt;
       console.log(page);
 
-        let offset = itemsPerPage * (page - 1);
-        console.log(offset);
-        try {
-          const res = await api.getAllCharacters({
-            // comics: final,
-            limit: itemsPerPage,
-            offset: offset,
-            nameStartsWith: query,
-          });
-          // pagination.reset(res.total);
-          headerFindResult.innerHTML = '';
-          // Loading.remove();
-          createGallery(result.results);
-          // console.log(res.total);
-        } catch (error) {
-          console.log('Error!!!!!!!!!!!');
-        }
-      });
+      let offset = itemsPerPage * (page - 1);
+      console.log(offset);
+      try {
+        const res = await api.getAllCharacters({
+          // comics: final,
+          limit: itemsPerPage,
+          offset: offset,
+          nameStartsWith: query,
+        });
+        // pagination.reset(res.total);
+        headerFindResult.innerHTML = '';
+        // Loading.remove();
+        createGallery(result.results);
+        // console.log(res.total);
+      } catch (error) {
+        console.log('Error!!!!!!!!!!!');
+      }
+    });
 
-      // console.log(res.total);
-    } catch (error) {
-      console.log('Error!!!!!!!!!!!');
-    }
-
-    
+    // console.log(res.total);
+  } catch (error) {
+    console.log('Error!!!!!!!!!!!');
+  }
 }
 
 refs.paginationEl.classList.add('is-hidden');
@@ -142,19 +135,19 @@ window.addEventListener('scroll', () => {
 
   if (!scrollY == 0) {
     headerColor.classList.add('header-color');
+  } else {
+    headerColor.classList.remove('header-color');
   }
-  else {headerColor.classList.remove('header-color');}
-} )
-
+});
 
 function createGallery(data) {
   headerFindResult.insertAdjacentHTML('beforeend', createMarkup(data));
 }
 
 function createMarkup(array) {
-  
-  return array.map(el => { 
-      return`
+  return array
+    .map(el => {
+      return `
       <a class="gallery__link" href="#">
         <div class="gallery-item"">
         <img data-set="${el.id}"
@@ -169,6 +162,6 @@ function createMarkup(array) {
         </div>
       </a>
     `;
-  })
-  .join('');
-  }
+    })
+    .join('');
+}
