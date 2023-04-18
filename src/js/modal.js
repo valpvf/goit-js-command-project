630844c8b2563d0cadd7bc26ae7847c9e3ac229b
M1keXl <mihayloh.com@gmail.com>
2023-04-18 11:39:51 +0300

    proba1

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,49 +1,56 @@
 import { api } from '../helpers/api';
-const characterCardEl = document.querySelector('.backdrop-two');
+
 const closeBtnEl = document.querySelector('.modal-close-btn');
-const modalTwoClose = document.querySelector('.backdrop-two');
+const modalOneClose = document.querySelector('.backdrop-one');
 const modalHeroEl = document.querySelector('.images');
 const comicsEl = document.querySelector('.description');
 const lastcomicssection = document.querySelector('.swiper-wrapper');
 function onCloseBtnElClick() {
-  modalTwoClose.classList.add('is-concealed');
+  modalOneClose.classList.add('is-concealed');
 }
 closeBtnEl.addEventListener('click', onCloseBtnElClick);
 lastcomicssection.addEventListener('click', onContainerClick);
 
 async function getComics(id) {
-    const comics1 = await api.getComicsById({ comicsId: id });
+    const comics1 = await api.getComicById({ comicsId: id });
     return comics1;
 }
-function onContainerClick(event) {
-  modalTwoClose.classList.remove('is-concealed');
+async function onContainerClick(event) {
+  modalOneClose.classList.remove('is-concealed');
   const id = event.target.dataset.id;
-  const comicsObject = getComics(id)
-    .then(res => {
-      const comicsIds = res[0].comics.items
-        .map(el => el.resourceURI)
-        .map(el => el.split('/'))
-        .map(el => el[el.length - 1]);
-      console.log(comicsIds);
+  const comicsObject = await getComics(id);
+  const charIds = comicsObject[0].characters.items
+    .map(el => el.resourceURI)
+    .map(el => el.split('/'))
+    .map(el => el[el.length - 1]);
 
-      const seriesIds = res[0].series.items
-        .map(el => el.resourceURI)
-        .map(el => el.split('/'))
-        .map(el => el[el.length - 1]);
+  const seriesIds = comicsObject[0].series.items
+    .map(el => el.resourceURI)
+    .map(el => el.split('/'))
+    .map(el => el[el.length - 1]);
+        const authorIds = comicsObject[0].creators.items
+    .map(el => el.resourceURI)
+    .map(el => el.split('/'))
+    .map(el => el[el.length - 1]);
+  for (let i = 0; i < 3; i += 1) {
+    const charId = charIds[i];
+    const seriesId = seriesIds[i];
+      const authorId = authorIds[i];
+    comicsObject[0][`charac${i}`] = await api.getComicById({ charId });
+    comicsObject[0][`series${i}`] = await api.getSeriesById({ seriesId });
+    comicsObject[0][`authors${i}`] = await api.getSeriesById({ authorId });
+  }
+  const markups = [createMarkupImages(comicsObject[0]), createMarkupText(comicsObject[0])];
 
-
-      const markups = [createMarkupImages(res[0]), createMarkupText(res[0])];
-      return markups;
-    })
-    .then(markup => {
-      modalHeroEl.innerHTML = markup[0];
-      comicsEl.innerHTML = markup[1];
-    });
+  modalHeroEl.innerHTML = markups[0];
+  comicsEl.innerHTML = markups[1];
 }
 
+  
+  
 
-function createMarkupImages(conicsd) {
-  const { id, thumbnail, name, description, modified, comics, creators } = conicsd;
+function createMarkupImages(comics) {
+  const { id, thumbnail, name, description, modified, comics, creators, series0, series1, series2  } = comics;
   const markup = `
       <img
         class="modal-main-img"
@@ -56,10 +63,10 @@ function createMarkupImages(conicsd) {
             <img class="photo-slide-image" src="${series0[0].thumbnail.path}.${series0[0].thumbnail.extension}" alt="crawl" data-id="${series0[0].id}" />
         </li>
         <li>
-            <img class="photo-slide-image" src="./img/modal2/image7.png" alt="talk" />
+            <img class="photo-slide-image" src="${series1[0].thumbnail.path}.${series1[0].thumbnail.extension}" alt="crawl" data-id="${series1[0].id}" />
         </li>
         <li>
-            <img class="photo-slide-image" src="./img/modal2/image8.png" alt="on-knee" />
+            <img class="photo-slide-image" src="${series2[0].thumbnail.path}.${series2[0].thumbnail.extension}" alt="crawl" data-id="${series2[0].id}" />
         </li>
     </ul>`
     ;
@@ -68,7 +75,7 @@ function createMarkupImages(conicsd) {
 }
 
 function createMarkupText(conicsd) {
-  const { id, thumbnail, name, description, modified, comics, creators } = conicsd;
+  const { id, thumbnail, name, description, modified, comics, creators, format, startYear, pageCount, price, authors0, charac0,charac1,charac2,charac3,charac4,charac5} = conicsd;
   const options = { month: 'long', day: 'numeric', year: 'numeric', };
   const unformattedDate = +Date.parse(modified);
   const dateString = new Date(unformattedDate);
@@ -78,7 +85,7 @@ function createMarkupText(conicsd) {
       <div class="desc-head">
         <h1 class="title">${name}</h1>
         <div class="head-inf">
-            <p class="riska"> <span class="c-author">${creators}</span>|<span class="c-date">${date}</span>
+            <p class="riska"> <span class="c-author">${authors0[0].items[0].name}</span>|<span class="c-date">${date}</span>
             </p>
        </div>
     </div>
@@ -88,34 +95,59 @@ function createMarkupText(conicsd) {
 <ul class="desc-list">
         <li class="list-item">
             <P class="list-punkt">FORMAT</P>
-            <P class="list-inf">Comic</P>
+            <P class="list-inf">${format}</P>
         </li>
         <li class="list-item">
             <P class="list-punkt">YEAR RELEASED</P>
-            <P class="list-inf">1967</P>
+            <P class="list-inf">${startYear}</P>
         </li>
         <li class="list-item">
             <P class="list-punkt">PAGES</P>
-            <P class="list-inf">28</P>
+            <P class="list-inf">${pageCount}</P>
         </li>
         <li class="list-item">
             <P class="list-punkt">PRICE</P>
-            <P class="list-inf">$3.99</P>
+            <P class="list-inf">${price}</P>
         </li>
     </ul>
     <h2 class="title author">Creator</h2>
     <div class="author-inf">
-        <img class="author-img" src="./img/modal2/image21.png" alt="one">
+        <img class="author-img" src="${authors0[0].thumbnail.path}.${authors0[0].thumbnail.extension}" alt="one" data-id="${authors0[0].id}>
         <div class="aut-desc">
-            <p class="author-prof">Writer</p>
-            <h3 class="author-name">${creators}</h3>
+            <p class="author-prof">${authors0[0].items[0].role}</p>
+            <h3 class="author-name">${authors0[0].items[0].name}</h3>
         </div>
     </div>
     <h2 class="title characters">Characters</h2>
     <ul class="characters-list">
         <li class="char-item">
-            <img class="character-img" src="./img/modal2/image19.png" alt="one" />
-            <h3 class="character-name">Daredevilic</h3>
+            <img class="character-img" src="${charac0[0].thumbnail.path}.${charac0[0].thumbnail.extension}" alt="one"data-id="${charac0[0].id} />
+            <h3 class="character-name">${charac0[0].items.name}</h3>
+
+        </li>
+         <li class="char-item">
+            <img class="character-img" src="${charac1[0].thumbnail.path}.${charac1[0].thumbnail.extension}" alt="one"data-id="${charac1[0].id} />
+            <h3 class="character-name">${charac1[0].items.name}</h3>
+
+        </li>
+         <li class="char-item">
+            <img class="character-img" src="${charac2[0].thumbnail.path}.${charac2[0].thumbnail.extension}" alt="one"data-id="${charac2[0].id}/>
+            <h3 class="character-name">${charac2[0].items.name}</h3>
+
+        </li>
+         <li class="char-item">
+            <img class="character-img" src="${charac3[0].thumbnail.path}.${charac3[0].thumbnail.extension}" alt="one"data-id="${charac3[0].id}/>
+            <h3 class="character-name">${charac3[0].items.name}</h3>
+
+        </li>
+         <li class="char-item">
+            <img class="character-img" src="${charac4[0].thumbnail.path}.${charac4[0].thumbnail.extension}" alt="one"data-id="${charac4[0].id}/>
+            <h3 class="character-name">${charac4[0].items.name}</h3>
+
+        </li>
+         <li class="char-item">
+            <img class="character-img" src="${charac5[0].thumbnail.path}.${charac5[0].thumbnail.extension}" alt="one"data-id="${charac5[0].id}/>
+            <h3 class="character-name">${charac5[0].items.name}</h3>
 
         </li>
 
