4b187b8eaa394dba167a28afc6347ea6fa6ca855
iDgoe <e.novykov@gmail.com>
2023-04-19 23:55:01 +0300

    header-css+pagination-css

--- src/js/header.js
+++ src/js/header.js
@@ -1,8 +1,5 @@
 import { api } from '../helpers/api';
-import datepicker from 'js-datepicker';
-import debounce from 'lodash.debounce';
 import 'js-datepicker/dist/datepicker.min.css';
-import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
 
@@ -16,7 +13,7 @@ const refs = {
 // let offset = 0;
 let itemsPerPage = null;
 let windowWidth = window.getComputedStyle(refs.mainContainer).width;
-console.log(windowWidth);
+
 
 // ВИЗНАЧАЄМО ШИРИНУ ВЬЮПОРТУ
 switch (windowWidth) {
@@ -35,22 +32,19 @@ switch (windowWidth) {
     break;
 }
 
-console.log(itemsPerPage);
-// НАЛАШТОВУЮ ПАГІНАЦІЮ
 
+// НАЛАШТОВУЮ ПАГІНАЦІЮ
 const paginationOptions = {
   totalItems: 0,
   itemsPerPage: itemsPerPage,
   visiblePages: 2,
   page: 1,
-  centerAlign: true,
 };
 
 const pagination = new Pagination(refs.paginationEl, paginationOptions);
 
 let query = '';
 let lightbox = null;
-// let page = 1;
 const perPage = 40;
 let result = [];
 
@@ -58,81 +52,70 @@ const headerInput = document.querySelector('.header-form');
 const headerColor = document.querySelector('.header');
 const headerFindResult = document.querySelector('.header-find');
 
-const characterOut = document.querySelector('.header-output');
-const headerIcon = document.querySelector('.header-icon');
-const headerBtn = document.querySelector('.header-btn');
+// const characterOut = document.querySelector('.header-output');
+// const headerIcon = document.querySelector('.header-icon');
+// const headerBtn = document.querySelector('.header-btn');
 
 async function addInput(event) {
   event.preventDefault();
   const { target: formEl } = event;
   query = formEl.elements.searchQuery.value;
   headerInput.reset();
-  console.log(query);
   // refs.paginationEl.classList.remove('is-hidden');
 
   if (query.trim() == '') {
     refs.paginationEl.classList.add('is-hidden');
-    console.log('Please specify your search query.');
     return;
   }
   // const result = await  api.getAllCharacters({nameStartsWith: query})
-  console.log(itemsPerPage);
   try {
     const result = await api.getAllCharacters({
-      // comics: final,
       limit: itemsPerPage,
       offset: 0,
       nameStartsWith: query,
     });
-    // pagination.reset(res.total);
-    console.log(itemsPerPage);
-    console.log(result.results.length);
+    
+   
     if (result.results.length == 0) {
       refs.paginationEl.classList.add('is-hidden');
-      // headerFindResult.innerHTML = '<span class="char-error-main"></span>';
       headerFindResult.innerHTML = '<span class="char-error"></span>';
-      console.log(
-      'Search result is zero. Change your query',
-      );
+   
         return;
       }
-      console.log('itemsPerPage====================');
+      
       headerFindResult.innerHTML = '';
       // Loading.remove();
 
       createGallery(result.results);
       pagination.reset(result.total);
       refs.paginationEl.classList.remove('is-hidden');
-      console.log('itemsPerPage====================');
+     
       pagination.on('beforeMove', async evt => {
       const { page } = evt;
-      console.log(page);
-
+    
         let offset = itemsPerPage * (page - 1);
-        console.log(offset);
+  
         try {
           const result = await api.getAllCharacters({
-            // comics: final,
+            
             limit: itemsPerPage,
             offset: offset,
             nameStartsWith: query,
           });
-          // pagination.reset(res.total);
+          
           headerFindResult.innerHTML = '';
           // Loading.remove();
           createGallery(result.results);
-          // console.log(res.total);
+          
         } catch (error) {
           console.log('Error!!!!!!!!!!!');
         }
       });
 
-    // console.log(res.total);
   } catch (error) {
     console.log('Error!!!!!!!!!!!');
   }
 }
-
 refs.paginationEl.classList.add('is-hidden');
 
 headerInput.addEventListener('submit', addInput);
@@ -162,7 +145,7 @@ function createMarkup(array) {
         alt=''
         class='header-find-img'
       />
-          <div class="info">
+          <div class="info-header">
           <h3 data-set="${el.id}" class='header-find-text'>${el.name}</h3>
           
           </div>
